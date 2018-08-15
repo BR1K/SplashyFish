80961ac554db3a60d890154789a10d549b49f4e6
Brikend Behrami <brikendbehrami@gmail.com>
2018-08-15 02:53:27 -0400

    add color to obstacles

--- dist/bundle.js
+++ dist/bundle.js
@@ -331,14 +331,17 @@ class Obstacle {
     this.width = 100;
     this.height = 0;
     this.space = 0;
-    this.color = this.getRandomColor();
 
     this.draw = this.draw.bind(this);
+    this.getRandomColor = this.getRandomColor.bind(this);
+    this.color = this.getRandomColor();
   }
 
 
   draw() {
-    debugger
+    // debugger
+    this.ctx.fillStyle = this.color;
+
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
 
     this.ctx.fillRect(this.x, this.height + this.space, this.width, this.canvas.height);
@@ -352,10 +355,12 @@ class Obstacle {
   }
 
   getRandomColor() {
+    // debugger
     const red = this.getRandomInt(0, 257);
     const green = this.getRandomInt(0, 257);
     const blue = this.getRandomInt(0, 257);
-    return "rgb('+r+','+g+','+b+')";
+    return `rgb(${red}, ${green}, ${blue})`;
+    debugger
   }
 
 
